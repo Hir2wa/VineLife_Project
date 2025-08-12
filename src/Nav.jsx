e4f1c31536e5aee266f435db53c30d7490156487
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-12 20:54:40 +0200

    first commit

---
+++ src/Nav.jsx
@@ -0,0 +1,16 @@
+import { text } from "express";
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div id="nav">
+      <p>#VANLIFE</p>
+      <div>
+        <p>About</p>
+        <p>Vans</p>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
