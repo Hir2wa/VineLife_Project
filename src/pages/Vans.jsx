21b7733a846fefe67f547bdea85eec1d5a9500ab
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-13 10:24:19 +0200

    added new component

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,23 +1,41 @@
 import React, { useEffect, useState } from "react";
 
-const data = () => {
-  const [data, setData] = useState();
+const Vans = () => {
+  const [vans, setVans] = useState([]);
+
   useEffect(() => {
     async function toFetch() {
-      const res = await fetch("/api/vans")
-        .json()
-        .catch((err) => {
-          throw new Error(" Error Fetching Data ");
-        });
-
-      return data;
+      try {
+        const res = await fetch("/api/vans");
+        const data = await res.json();
+        setVans(data.vans); // adjust key according to MirageJS output
+      } catch (err) {
+        console.error(err);
+      }
     }
-    toFetch().then((data) => setData());
+    toFetch();
   }, []);
-};
 
-const Vans = () => {
-  return <div>Vans page 🗃 </div>;
+  const vanElement = vans.map((van) => (
+    <div key={van.id} className="van-tile">
+      <img src={van.imageUrl} alt={van.name} />
+      <div className="van-info">
+        <h3>{van.name}</h3>
+        <p>
+          ${van.price}
+          <span>/day</span>
+        </p>
+      </div>
+      <i className={`van-type ${van.type} selected`}>{van.type}</i>
+    </div>
+  ));
+
+  return (
+    <div className="van-list-container">
+      <h1>Explore our van options</h1>
+      <div className="van-list">{vanElement}</div>
+    </div>
+  );
 };
 
 export default Vans;
