57560e4a9b8d780a0f7e29d5492b6d7bc84dce8d
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-13 09:51:00 +0200

    fetched data using api

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,4 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const data = () => {
+  const [data, setData] = useState();
+  useEffect(() => {
+    async function toFetch() {
+      const res = await fetch("/api/vans")
+        .json()
+        .catch((err) => {
+          throw new Error(" Error Fetching Data ");
+        });
+
+      return data;
+    }
+    toFetch().then((data) => setData());
+  }, []);
+};
 
 const Vans = () => {
   return <div>Vans page 🗃 </div>;
