10b606661c4e22895d1feff3ef34c4872538c23b
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-15 20:35:32 +0200

    commented existing codes

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,30 @@ import About from "./pages/About";
 import "./server";
 import Vans from "./pages/Vans";
 import VanDetails from "./pages/VanDetails";
+import Input from "./pages/Input";
+import Link1 from "./pages/Link1";
 
 function App() {
   return (
-    <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">
-          #VanLife
-        </Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetails />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <header>
+    //     <Link className="site-logo" to="/">
+    //       #VanLife
+    //     </Link>
+    //     <nav>
+    //       <Link to="/about">About</Link>
+    //       <Link to="/vans">Vans</Link>
+    //     </nav>
+    //   </header>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/about" element={<About />} />
+    //     <Route path="/vans" element={<Vans />} />
+    //     <Route path="/vans/:id" element={<VanDetails />} />
+    //   </Routes>
+    // </BrowserRouter>
+    // <Input />
+    <Link1 />
   );
 }
 
